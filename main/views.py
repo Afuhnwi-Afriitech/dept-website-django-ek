7e4bf1d71a2d6fb9ad9c686671254a8e5be934d1
elroykanye <elroykanye@gmail.com>
2021-06-18 00:05:40 +0100

    updated more views as there were nav items

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import BadHeaderError, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import ListView
@@ -8,12 +10,24 @@ def home_page(request):
     return render(request, 'index.html')
 
 
+def profile(request):
+    return render(request, 'profile.html')
+
+
 def features(request):
     return render(request, 'features.html')
 
 
+def courses(request):
+    return render(request, 'courses.html')
+
+
 def faq(request):
     return render(request, 'faq.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
 
 
 def about_us(request):
@@ -22,3 +36,25 @@ def about_us(request):
 
 def contact_us(request):
     return render(request, 'contact-us.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = request.POST
+        #if form.is_valid():
+        # subject = "Website Inquiry"
+        subject = form['subject']
+        body = {
+            'name': form['name'],
+            'email': form['email'],
+            'message': form['message'],
+        }
+        message = "\n".join(body.values())
+
+        try:
+            send_mail(subject, message, 'elroykanye@gmail.com', ['elroykanye@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return redirect("main:homepage")
+    form = request.POST
+    return render(request, "contact-us.html", {'form': form})

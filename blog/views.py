77f87e172dcd60aa0fcf1e933b4312b5a72cfa28
elroykanye <elroykanye@gmail.com>
2021-06-18 00:05:56 +0100

    updated view for BlogDetail

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic import ListView
 
 from blog.models import Post
@@ -13,4 +13,7 @@ class BlogListView(ListView):
     template_name = 'blog.html'
 
 
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog-post.html'
 

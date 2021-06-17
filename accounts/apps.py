40cb1bda4ac7c13256078a3f5de543ba6090bcb6
elroykanye <elroykanye@gmail.com>
2021-06-17 15:51:44 +0100

    moved dir

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EkAuthConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ek_auth'
+    name = 'accounts'

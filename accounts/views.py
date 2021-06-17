fec2810a3ca24ef654c8719d998bfecd30193cee
elroykanye <elroykanye@gmail.com>
2021-06-17 15:58:20 +0100

    added all required views from nav menu

---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        try:
+            User.objects.get(username=request.POST.get('username'))
+            return render(request, 'registration.html', {
+                'error': 'Username is already taken'
+            })
+        except User.DoesNotExist:
+            user = User.objects.create_user(
+                request.POST['username'],
+                password=request.POST['password']
+            )
+            auth.login(request, user)
+            return redirect('index')
+    else:
+        return render(request, 'registration.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(
+            username=request.POST.get('username'),
+            password=request.POST.get('password')
+        )
+
+        if user is None:
+            auth.login(request, user)
+            request.session['user_id'] = user
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {
+                'error': 'Username or password is incorrect'
+            })
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+
+        try:
+            del request.session['user_id']
+        except KeyError:
+            pass
+        auth.logout(request)
+    print(request.session['user_id'])
+    try:
+        del request.session['user_id']
+    except KeyError:
+        pass
+    return redirect('index')
--- main/views.py
+++ main/views.py
@@ -1,5 +1,3 @@
-
-
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,3 +6,19 @@ from django.views.generic import ListView
 
 def home_page(request):
     return render(request, 'index.html')
+
+
+def features(request):
+    return render(request, 'features.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
+
+
+def about_us(request):
+    return render(request, 'about-us.html')
+
+
+def contact_us(request):
+    return render(request, 'contact-us.html')

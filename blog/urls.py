6fff57dbfc53e371526daa785303f1c5ef1285ee
elroykanye <elroykanye@gmail.com>
2021-06-18 00:06:47 +0100

    added regex url pattern for blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('blog', BlogListView.as_view(), name='blog')
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='blog_post'),
+    path('', BlogListView.as_view(), name='blog')
 ]

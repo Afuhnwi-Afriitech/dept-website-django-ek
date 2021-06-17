e6495c1c1585a4f68e850e44d216c5d68f1e86e4
elroykanye <elroykanye@gmail.com>
2021-06-17 15:59:01 +0100

    added support for accounts

--- dept_webapp/settings.py
+++ dept_webapp/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
 ]
 
 MIDDLEWARE = [

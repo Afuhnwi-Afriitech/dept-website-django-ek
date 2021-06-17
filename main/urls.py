05b82b1345781fe6dcae8c50b669c8c8e3f41855
elroykanye <elroykanye@gmail.com>
2021-06-17 15:57:27 +0100

    added links to all nav pages

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from main.views import home_page
+from main.views import *
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='index'),
+    path('features', features, name='features'),
+    path('faq', faq, name='faq'),
+    path('about-us', about_us, name='about-us'),
+    path('contact-us', contact_us, name='contact-us'),
 ]

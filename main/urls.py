f50f59e66e81bcf241f8c9558c58d01181969027
elroykanye <elroykanye@gmail.com>
2021-06-18 00:06:13 +0100

    updated urls as there were views

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('faq', faq, name='faq'),
     path('about-us', about_us, name='about-us'),
     path('contact-us', contact_us, name='contact-us'),
+    path('gallery', gallery, name='gallery'),
+    path('courses', courses, name='courses'),
+    path('profile', profile, name='profile'),
+    path('contact', contact, name='contact'),
 ]
